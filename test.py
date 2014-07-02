98a8253f521b0e2d689be28bb79ba06cbdd1cc53
Andrea Stocco <gardoma@users.noreply.github.com>
2014-07-01 23:13:18 -0700

    Update and rename ops.py to test.py

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
+# Test.py: A simple test of Spyne's capabilities
+
 import spyne.gui.visualize
 import spyne.gui.gui
 import spyne.model
 
-model=spyne.model.Model()
+model = spyne.model.Model()
 
 spyne.gui.gui.Show(model)
